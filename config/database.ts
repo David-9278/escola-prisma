d8be74637b628be90e88b2b57dc8c7c7b6da35f5
David-9278 <dls32@aluno.ifal.edu.br>
2026-05-01 17:36:13 -0300

    Create database.ts

---
+++ config/database.ts
@@ -0,0 +1,8 @@
+// config/database.ts
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaClient } from "../generated/prisma/client";
+
+const adapter = new PrismaBetterSqlite3({ url: './dev.db' });
+const prisma = new PrismaClient({ adapter, log: ['query'] });
+
+export { prisma };
